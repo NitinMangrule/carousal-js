0c5b0a82ad7e95b9f6632a3e8c63575daaa523d0
Nitin Mangrule <ndmangrule@gmail.com>
2021-01-02 21:00:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// console.log(Math.max()); // -Infinity
-// console.log(Math.max(1,2,3,4));
 
-// console.log(Math.min());
-// console.log(Math.min(1,2,3,4,-5));
-
-
-// Shodow Dom
-
-
-// var host = document.querySelector('#shadowHost');
-
-// var shadowRoot = host.attachShadow({ mode: 'open'});
-
-// var div = document.createElement('div');
-// div.textContent = "Can you see me now";
-// div.className = "x";
-
-// shadowRoot.appendChild(div);
 
 // Code for carousel using Javascript
 
